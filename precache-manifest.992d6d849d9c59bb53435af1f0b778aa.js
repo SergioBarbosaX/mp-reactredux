ed69fcf893bb1af09bea2ab6b1718aeb887d48ec
SergioBarbosaX <serbarbos@hotmail.com>
2019-10-04 21:53:21 +0200

    Updates

---
+++ precache-manifest.992d6d849d9c59bb53435af1f0b778aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a5beeb74d70786e622dd868f8c8b05",
+    "url": "/mp-reactredux/index.html"
+  },
+  {
+    "revision": "0a03c05c2bbdda8270d3",
+    "url": "/mp-reactredux/static/css/main.cb9714b0.chunk.css"
+  },
+  {
+    "revision": "c30a7896aa340c24180a",
+    "url": "/mp-reactredux/static/js/2.623af575.chunk.js"
+  },
+  {
+    "revision": "0a03c05c2bbdda8270d3",
+    "url": "/mp-reactredux/static/js/main.b76089b8.chunk.js"
+  },
+  {
+    "revision": "39763bad10e55b7e09dd",
+    "url": "/mp-reactredux/static/js/runtime~main.1d056ced.js"
+  }
+]);
